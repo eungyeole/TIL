62e306b8dd828a793af68a9ad3c5b7f15dc62640
Eungyeole <eungyeole@naver.com>
2021-06-15 19:07:43 +0900

    create Header

---
+++ packages/frontend/src/base/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import * as S from './styles'
+const Header : FC = () => {
+    return(
+        <S.Block>
+            
+        </S.Block>
+    )
+}
+export default Header;
