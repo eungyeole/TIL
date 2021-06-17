b3c92051e500b17e8d19ba93ba5a7d67222b1d01
Eungyeole <eungyeole@naver.com>
2021-06-17 16:56:12 +0900

    Update : Header 반응형 추가

--- packages/frontend/src/components/base/Header/Header.tsx
+++ packages/frontend/src/components/base/Header/Header.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
 import * as S from './styles'
+import HeaderLogo from "./HeaderLogo/HeaderLogo";
+import HeaderUserIcon from "./HeaderUserIcon/HeaderUserIcon";
+import HeaderUserMenu from "./HeaderUserMenu/HeaderUserMenu";
 const Header : FC = () => {
     return(
         <S.Block>
-            
+            <S.Inner>
+                <HeaderLogo username="eungyeole"></HeaderLogo>
+                <S.Right>
+                    <HeaderUserIcon url="https://avatars.githubusercontent.com/u/66717787?v=4"></HeaderUserIcon>
+                    <HeaderUserMenu visible={true}></HeaderUserMenu>
+                </S.Right>
+            </S.Inner>
         </S.Block>
     )
 }
--- packages/frontend/src/components/base/Header/styles.ts
+++ packages/frontend/src/components/base/Header/styles.ts
@@ -1,4 +1,16 @@
 import styled from "@emotion/styled";
+import MainResponsive from "../../main/MainResponsive/MainResponsive";
 export const Block = styled.div`
-
+    height: 50px;
+`
+export const Inner = styled(MainResponsive)`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+export const Right = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
 `
