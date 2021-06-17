d92c02e4ed06b9483a33afc5917fc2b0be8cd4d1
Eungyeole <eungyeole@naver.com>
2021-06-17 16:55:33 +0900

    Update : BlockQuote 로직수정

--- packages/frontend/src/components/common/MarkdownRender/BlockQuote/BlockQuote.tsx
+++ packages/frontend/src/components/common/MarkdownRender/BlockQuote/BlockQuote.tsx
@@ -7,13 +7,24 @@ interface BlockQuoteProps {
 }
 const BlockQuote : FC<BlockQuoteProps> = ({children}) => {
     useEffect(()=>{
-        console.log()
+        //console.log(children[1])
+        console.log(children[1]?.props?.children.map((i)=>{
+            if(typeof i === "object") return i;
+            else return i.split("\n").map(line=>{
+                if(line.length > 0) return (<span><br/>{line}</span>)
+            })
+            
+        }))
     },[children])
     return(
         <S.Block>
-            {children[1]?.props.children[0].split('\n').map((line,index) => {
-                return (<span key={index}>{line}<br/></span>)
-            })}
+            {
+                children[1]?.props?.children.map((i)=>{
+                    if(typeof i === "object") return i;
+                    else return i.split("\n").map(line=><span><br/>{line}</span>)
+                    
+                })
+            }
         </S.Block>
     )
 }
