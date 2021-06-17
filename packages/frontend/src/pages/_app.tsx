dec076494718ca163f00b0df9a8e92f13ff4b2b8
Eungyeole <eungyeole@naver.com>
2021-06-17 21:51:32 +0900

    Update : Header 글로벌 설정

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import Header from "../components/base/Header/Header"
 import TILThemeProvider from "../styles/TILThemeProivder"
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <TILThemeProvider>
+      <Header></Header>
       <Component {...pageProps} />
     </TILThemeProvider>
   )
