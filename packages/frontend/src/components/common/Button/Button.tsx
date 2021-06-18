5ff21211252149e1c6a85d6e196e917d2fa5c668
Eungyeole <eungyeole@naver.com>
2021-06-18 11:59:14 +0900

    Update : Button 컴포넌트 기능추가

--- packages/frontend/src/components/common/Button/Button.tsx
+++ packages/frontend/src/components/common/Button/Button.tsx
@@ -1,7 +1,23 @@
-interface ButtonProps{}
-const Button: React.FC<ButtonProps> = () => {
+import { HTMLProps } from 'react'
+import * as S from './styles'
+interface ButtonProps extends HTMLProps<HTMLButtonElement> {}
+const Button: React.FC<ButtonProps> = ({
+    children,
+    ...rest
+}) => {
+    const htmlProps = rest as any;
     return(
-        <div></div>
+        <S.Block
+            {...htmlProps}
+            onClick={e => {
+                if (htmlProps.onClick) {
+                  htmlProps.onClick(e);
+                }
+                (e.target as HTMLButtonElement).blur();
+            }}
+        >
+            {children}
+        </S.Block>
     )
 }
 export default Button
--- packages/frontend/src/components/common/Button/styles.ts
+++ packages/frontend/src/components/common/Button/styles.ts
@@ -1,4 +1,23 @@
 import styled from "@emotion/styled";
-export const ButtonWrapper = styled.div`
-
+export const Block = styled.button`
+    background: #000000;
+    color: white;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 25px;
+    font-size: 14px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: 0.2s;
+    border: 1px solid black;
+    &:hover,
+    &:focus {
+        background: white;
+        color : black;
+        border: 1px solid black;
+    }
 `
