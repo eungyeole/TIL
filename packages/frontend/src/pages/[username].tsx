609d72c85d25461a9ad49bb3bfe45c5aa60332a9
Eungyeole <eungyeole@naver.com>
2021-06-18 12:00:56 +0900

    Chore : username 페이지, til .gitkeep 추가

--- packages/frontend/src/pages/[username].tsx
+++ packages/frontend/src/pages/[username].tsx
@@ -1,17 +1,33 @@
 import { GetServerSidePropsContext } from "next";
 import { NextPage } from "next";
-import Header from "../components/base/Header/Header";
-
+import MainResponsive from "../components/main/MainResponsive/MainResponsive";
+import TILHeader from "../components/til/TILHeader/TILHeader";
+import WorkSpaceItem from "../components/til/WorkSpaceItem/WorkSpaceItem";
+import styled from '@emotion/styled'
+import WorkSpaceList from "../components/til/WorkSpaceList/WorkSpaceList";
 interface UserPageProps {
     username : string
 }
 const UserPage: NextPage<UserPageProps> = ({username}) => {
     return(
         <> 
-            {username}
+            <TILHeader username={username}></TILHeader>
+            <Block>
+                <Inner>
+                    <WorkSpaceList></WorkSpaceList>
+                </Inner>
+            </Block>
         </>
     )
 }
+const Block = styled.div`
+    background : #fafafa;
+    min-height: 700px;
+`
+const Inner = styled(MainResponsive)`
+    transform: translateY(-35px);
+`
+
 export async function getServerSideProps(context : GetServerSidePropsContext){
     const { username } = context.query;
     return {
