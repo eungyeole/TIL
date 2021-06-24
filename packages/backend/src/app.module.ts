2d49f16c2efb24d0e4b457f98b946e60351665cb
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-24 15:59:44 +0900

    ⚡ :: ConfigService Provider 추가

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
