d1ef0dc606f96c74f6b781db0c88d424bcc8ffcc
leeseojune <sung07288346@gmail.com>
2021-06-24 15:27:09 +0900

    ⚡ :: app.module add userModule

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
