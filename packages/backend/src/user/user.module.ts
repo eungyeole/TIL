a7a5af30b5d9294f80464e9863acde1af0ec23c0
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-24 16:43:33 +0900

    🐛 :: ConfigService

--- packages/backend/src/user/user.module.ts
+++ packages/backend/src/user/user.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
     imports: [ConfigModule.forRoot()],
-  controllers: [UserController],
-  providers: [UserService],
+    controllers: [UserController],
+    providers: [UserService, ConfigService],
 })
 export class UserModule {
     
