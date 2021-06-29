21a718d8da24354fce27efce0b94b2dcd65ae83a
Eungyeole <eungyeole@naver.com>
2021-06-29 11:43:44 +0900

    fix: prerendering error

--- packages/frontend/src/pages/[username]/[workspaceName].tsx
+++ packages/frontend/src/pages/[username]/[workspaceName].tsx
@@ -1,10 +1,25 @@
-import { NextPage } from "next";
+import { GetServerSidePropsContext, NextPage } from "next";
 
-const WorkSpacePage: NextPage = () => {
+interface WorkSpacePageProps{
+    workspacename : string
+}
+
+const WorkSpacePage: NextPage<WorkSpacePageProps> = ({workspacename}) => {
     return(
         <div>
+            {workspacename}
             워크스페이스
         </div>
     )
 }
+
+export async function getServerSideProps(context : GetServerSidePropsContext){
+    const { workspacename } = context.query;
+    return {
+        props : {
+            workspacename : workspacename
+        }
+    }
+}
+
 export default WorkSpacePage;
