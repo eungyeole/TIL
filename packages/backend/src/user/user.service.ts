2137359207165678738859e2d020267c03da69da
leeseojune <sung07288346@gmail.com>
2021-07-05 23:53:31 +0900

    Update UserService

--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -1,12 +1,16 @@
 import { UnauthorizedException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import axios, { AxiosResponse } from 'axios';
-import { GithubCodeDto } from './user.dto';
+import { GithubCodeDto } from './dto/code.dto';
+import { TokenDto } from './dto/token.dto';
+import { User } from './entity/user.entity';
+import { UserRepository } from './entity/user.repo';
 
 export interface IGithubUserTypes {
-    githubId: string;
-    avator: string;
+    id: string;
+    profileImage: string;
     name: string;
     description: string;
     location: string;
@@ -16,10 +20,12 @@ export interface IGithubUserTypes {
 export class UserService {
 
     constructor(
-        private readonly configService: ConfigService
+        private readonly configService: ConfigService,
+        private readonly userRepository: UserRepository,
+        private readonly jwtService: JwtService,
     ) {}
 
-    public async getGithubInfo({ code }: GithubCodeDto): Promise<IGithubUserTypes> {
+    public async register({ code }: GithubCodeDto): Promise<TokenDto> {
         const getTokenUrl: string = 'https://github.com/login/oauth/access_token';
 
         const request = {
@@ -48,7 +54,17 @@ export class UserService {
             },
         });
 
-        const { login: githubId, avator_url: avator, name, bio: description, company: location } = data;
-        return { githubId, avator, name, description, location };
+        const { login: id, avatar_url: profileImage, name, bio: description, company: location } = data;
+
+        let user: User;
+        
+        if(await this.userRepository.findById(id) === undefined){
+            user = await this.userRepository.saveUser({id, profileImage, name, description, location});
+        }else user = await this.userRepository.findById(id);
+
+        //토큰 제작
+        return {
+            accessToken: this.jwtService.sign({id})
+        }
     }
 }
