b4f43bc5421bca351d3e5a1ece698843795201e1
leeseojune <sung07288346@gmail.com>
2021-07-05 23:51:20 +0900

    Update UserModule

--- packages/backend/src/user/user.module.ts
+++ packages/backend/src/user/user.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './entity/user.repo';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forFeature([UserRepository]),
+        JwtModule.register({
+            secret: 'testkey',
+            signOptions: {expiresIn: '120m'}
+        }),
+    ],
     controllers: [UserController],
-    providers: [UserService, ConfigService],
+    providers: [
+        UserService,
+    ],
 })
 export class UserModule {
     
