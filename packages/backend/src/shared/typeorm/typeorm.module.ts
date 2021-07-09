216b51f05d2ce3fa4e2a131561a7787d0b607d8d
leeseojune <sung07288346@gmail.com>
2021-07-09 19:39:40 +0900

    Update DB설정

--- packages/backend/src/shared/typeorm/typeorm.module.ts
+++ packages/backend/src/shared/typeorm/typeorm.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '../config/config.service';
 import { ConfigModule } from '../config/config.module';
+import { User } from 'src/user/entity/user.entity';
 
 @Module({
     imports: [
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
-            useFactory: (config: ConfigService) => config.get("MODE"),
+            useFactory: (config: ConfigService) => ({
+                type: "mysql",
+                entities: [User],
+                database: config.get("DEVELOPMENT_DATABASE_NAME"),
+                host: config.get("DEVELOPMENT_DATABASE_HOST"),
+                password: config.get("DEVELOPMENT_DATABASE_PASSWORD"),
+                port: config.get("DEVELOPMENT_DATABASE_PORT"),
+                username: config.get("DEVELOPMENT_DATABASE_USER"),
+                synchronize: true,
+                logging: true,
+            }),
             inject: [ConfigService],
         }),
     ],
